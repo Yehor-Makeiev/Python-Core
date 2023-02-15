7aab4103b66d4d81b7bbc8ef5a6ef344e66efa24
YehorMakeiev <yehor.makeiev@gmail.com>
2023-02-15 17:39:54 +0200

    in progress

---
+++ HW/hw4_7.py
@@ -0,0 +1,25 @@
+points = {
+    (0, 1): 2,
+    (0, 2): 3.8,
+    (0, 3): 2.7,
+    (1, 2): 2.5,
+    (1, 3): 4.1,
+    (2, 3): 3.9,
+}
+
+
+def calculate_distance(coordinates):
+    print(type(coordinates))
+
+
+
+calculate_distance([0, 1, 3, 2, 0])
+
+
+
+
+
+
+
+
+
